bd7bbebefea1fc9caae33cc33162b5b6849784fd
filipemedeiros12 <filipemedeiros144@gmail.com>
2021-05-29 11:59:23 -0300

    primeiro

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcular() {
+    var vlr1 = parseInt(document.getElementById('primeiroValor').value);
+    var vlr2 = parseInt(document.getElementById('segundoValor').value);
+    var vlr3 = parseInt(document.getElementById ('terceiroValor').value);
+    document.getElementById('resultado').value = (vlr1 + vlr2 + vlr3);
+}
+function multiplicar() {
+    var vlr1 = parseInt(document.getElementById('primeiroValor1').value);
+    var vlr2 = parseInt(document.getElementById('segundoValor1').value);
+    var vlr3 = parseInt(document.getElementById ('terceiroValor1').value);
+    document.getElementById('resultado1').value = (vlr1 * vlr2 * vlr3);
+}
+function dividir() {
+    var vlr1 = parseInt(document.getElementById('primeiroValor2').value);
+    var vlr2 = parseInt(document.getElementById('segundoValor2').value);
+    var vlr3 = parseInt(document.getElementById ('terceiroValor2').value);
+    document.getElementById('resultado2').value = (vlr1 / vlr2 / vlr3);
+}
