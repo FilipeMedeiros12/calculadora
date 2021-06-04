9130d34bcc1bdf4b84f0479f126d62645295985c
filipemedeiros12 <filipemedeiros144@gmail.com>
2021-06-04 17:14:31 -0300

    filipe

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ function dividir() {
     var vlr3 = parseInt(document.getElementById ('terceiroValor2').value);
     document.getElementById('resultado2').value = (vlr1 / vlr2 / vlr3);
 }
+function subtrair() {
+    var vlr1 = parseInt(document.getElementById('primeiroValor3').value);
+    var vlr2 = parseInt(document.getElementById('segundoValor3').value);
+    var vlr3 = parseInt(document.getElementById ('terceiroValor3').value);
+    document.getElementById('resultado3').value = (vlr1 - vlr2 - vlr3);
+}
